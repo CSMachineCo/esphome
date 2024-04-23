d21286fd0657a7314934da6c4848910c0027b8aa
Michael Primm <mprimm@gmail.com>
2024-04-23 09:58:30 -0400

    n

--- components/lora_signal/lora_signal_sensor.h
+++ components/lora_signal/lora_signal_sensor.h
@@ -3,7 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/core/helpers.h"
 #include "esphome/components/sensor/sensor.h"
-#include "esphome/components/lora_uart.h"
+#include "../components/lora_uart.h"
 
 namespace esphome {
 namespace lora_signal {
